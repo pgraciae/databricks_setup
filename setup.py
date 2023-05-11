f4c1e21e252121d3cceb3cc4cb69b2f791dc0e39
pgraciae <pgraciae>
2023-05-11 11:19:21 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+display(spark.sql("SHOW STORAGE CREDENTIALS"))
+
+# COMMAND ----------
+
+display(spark.sql("DESCRIBE STORAGE CREDENTIAL adlg2test01_cred"))
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE EXTERNAL LOCATION adlg2test01
+# MAGIC URL 'abfss://metastoredb@adlg2test01.dfs.core.windows.net/datalake'
+# MAGIC WITH (STORAGE CREDENTIAL adlg2test01_cred);
+
+# COMMAND ----------
+
+display(spark.sql("DESCRIBE EXTERNAL LOCATION adlg2test01"))
